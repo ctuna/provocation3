041cd8ae027d2f76d4e1b9fb53f690b3ee316132
Claire Tuna <tuna@Bjoern-Hartmanns-MacBook-Pro.local>
2014-04-30 22:30:26 -0700

    works for 5 words

--- public/helper.js
+++ public/helper.js
@@ -1,16 +1,39 @@
-    
-var wordIndex = 1; 
+var wordIndex = 0; 
+var MAX_WORD_LENGTH = 9
+var numWords = 5;
+
       document.onkeypress = function(evt) {
         evt = evt || window.event;
         var charCode = evt.keyCode || evt.which;
         var charStr = String.fromCharCode(charCode);
         console.log(charStr);
         var lastString = $("#" + wordIndex).text();
-        $("#" + wordIndex).text(lastString + charStr);
+        if (charCode == 8){
+        	$("#" + wordIndex).text(lastString.substring(0, lastString.length - 1));
+        }
+        else if (evt.keyCode == 13){
+
+        	nextWord();
+        }
+        else if (lastString.length<MAX_WORD_LENGTH){
+        	$("#" + wordIndex).text(lastString + charStr);
+        	}
       }
+    
 
 
+function nextWord(){
+	console.log("wordIndex was : " + wordIndex);
+	wordIndex = (wordIndex+1)%numWords;
+	console.log("wordIndex changed to: " + wordIndex);
+	if (wordIndex == 0){
+		clearAll();
+	}
+}
 
 
-      //clear function
-      //start new word
+function clearAll(){
+	for (var i = 0 ; i < numWords;i++){
+		$("#" + i).text("");
+	}
+}
