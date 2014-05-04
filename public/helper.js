b5f5fbb87a87cd3e9cd178da9ada0bfebf62c845
Claire Tuna <tuna@Bjoern-Hartmanns-MacBook-Pro.local>
2014-05-03 22:55:38 -0700

    reading RFIDs, needs communication when user is allowed to type

--- public/helper.js
+++ public/helper.js
@@ -10,74 +10,95 @@ var StatusEnum = {
     TYPING : 2
 }
 var currentStatus = StatusEnum.READY; 
-var scanStart;
-var timeout;
+
+//RFID SCANNING
+//time when scan started
+var scanStartMillis;
+
+
+//seconds we'll wait
+var SCAN_TIMEOUT= 200;
+//RFID so far
 var RFID = "";
-var minutes;
-var seconds;
+
+
 
 
 //KEY PRESS CAN EITHER BE RFID OR KEYBOARD INPUT 
       document.onkeypress = function(evt) {
+
         evt = evt || window.event;
         var charCode = evt.keyCode || evt.which;
-        //Grab Keyboard Input
+        //GRAB INPUT
         var charStr = String.fromCharCode(charCode);
-
-        console.log(charStr);
         //READ RFID
         if (currentStatus == StatusEnum.READY || currentStatus == StatusEnum.SCANNING){
           tryScan(charStr);
         }
-        //READ TEXT 
+       
         else if (currentStatus == StatusEnum.TYPING){
-          enterText(evt, charCode, charStr);
+          focusLine(wordIndex);
+          $('#caption').show();
+          if (charCode === 13) {
+            $("#caption").hide();
+            nextWord();
+          }
         }
-        //Grab current text inside input field
+
         
       }
 
-function enterText(evt, charCode, charStr){
-  console.log("in enter text");
-  var lastString = $("#" + wordIndex).val();
-        //is key a backspace?
-        if (charCode === 8){
-          $("#" + wordIndex).text(lastString.substring(0, lastString.length - 1));
-        }
-        //is key a return?
-        else if (evt.keyCode === 13){
-          nextWord();
-        }
-        //update text in input
-        else {
-          $("#" + wordIndex).value = lastString + charStr;
-        }
+
+
+function focusLine(num){
+
+  $('#'+num).show();
+  $('#'+num).focus();
+
+
+}
+
+
+
+function hideLine(num){
+    $("#" + num).val("");
+    $("#" + num).hide()
 }
 
 function tryScan(charStr){
-    console.log("in try scan");
-
+  var currentDate = new Date(); 
   if (isNumber(charStr)){
     //START SCANNING
     if (currentStatus == StatusEnum.READY){
-      var currentDate = new Date(); 
-      minutes = currentDate.getMinutes();
-      seconds = currentDate.getSeconds();
+
+      scanStartMillis = currentDate.getTime();
       RFID = RFID + charStr; 
       currentStatus = StatusEnum.SCANNING; 
-      console.log("STARTING SCAN");
     }
     else if (currentStatus == StatusEnum.SCANNING){
       RFID = RFID + charStr; 
-      console.log("SCANNING: RFID = " + RFID);
-      if (RFID.length == 5){
-        //SCANNED ENOUGH
-        currentStatus = StatusEnum.TYPING;
+      var scanEndMillis = currentDate.getTime();
+      var scanTime = scanEndMillis - scanStartMillis;
+      if (scanTime < SCAN_TIMEOUT){
+        if (RFID.length == 5){
+          currentStatus = StatusEnum.TYPING;
+          scanned();
+        }
+      }
+      //RESTART SCANNING IF TIMEOUT 
+      else {
+        currentStatus = StatusEnum.READY;
+        RFID = "";
+        }
+       
       }
     }
   }
-}
 
+//called when the RFID has been scanned and the user can type in the next input
+function scanned(){
+  console.log("SCANNED");
+}
 function isNumber(n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
 }
@@ -87,12 +108,12 @@ function nextWord(){
   //RESET 
   currentStatus = StatusEnum.READY;
   RFID="";
-  
-	console.log("wordIndex was : " + wordIndex);
 	wordIndex = (wordIndex+1)%numWords;
-  $("#" + wordIndex).show();
-  $("#" + wordIndex).focus();
-	console.log("wordIndex changed to: " + wordIndex);
+
+
+  focusLine(wordIndex);
+  hideLine(wordIndex);
+
   //if we've looped back to beginning, clear all words
 	if (wordIndex === 0){
 		clearAll();
@@ -105,5 +126,5 @@ function clearAll(){
 		$("#" + i).val("");
     $("#" + i).hide()
 	}
-  $("#0").show();
+  //$("#0").show();
 }
