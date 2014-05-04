f78289256d8b24acd254c19991c97beb014267d8
Claire Tuna <tuna@Bjoern-Hartmanns-MacBook-Pro.local>
2014-05-03 23:41:26 -0700

    working with robot reading sentence after entered

--- public/helper.js
+++ public/helper.js
@@ -9,24 +9,39 @@ var StatusEnum = {
     SCANNING : 1,
     TYPING : 2
 }
+var BACKSPACE = 8;
 var currentStatus = StatusEnum.READY; 
+
+//manually keep track of every string
+var sentence = [];
 
 //RFID SCANNING
 //time when scan started
 var scanStartMillis;
 
 
-//seconds we'll wait
-var SCAN_TIMEOUT= 200;
+//seconds we'll wait, 200 is real with cal id reader 
+//var SCAN_TIMEOUT= 200;
+var SCAN_TIMEOUT= 40000;
 //RFID so far
 var RFID = "";
 
 
+//for backspace detection 
+document.onkeydown = function(evt) {
+   evt = evt || window.event;
+   var charCode = evt.keyCode || evt.which;
+  if (currentStatus == StatusEnum.TYPING){
+    if (charCode == BACKSPACE){
+      sentence[wordIndex] = sentence[wordIndex].substring(0, sentence[wordIndex].length-1);
+      }
+  }
+}
 
 
 //KEY PRESS CAN EITHER BE RFID OR KEYBOARD INPUT 
       document.onkeypress = function(evt) {
-
+      
         evt = evt || window.event;
         var charCode = evt.keyCode || evt.which;
         //GRAB INPUT
@@ -37,7 +52,12 @@ var RFID = "";
         }
        
         else if (currentStatus == StatusEnum.TYPING){
+
           focusLine(wordIndex);
+
+          sentence[wordIndex]+=charStr;
+          
+
           $('#caption').show();
           if (charCode === 13) {
             $("#caption").hide();
@@ -48,7 +68,14 @@ var RFID = "";
         
       }
 
+function readSentence(){
 
+  var fullSentence = "";
+  for (var i = 0 ; i < sentence.length;i++){
+    fullSentence += sentence[i];
+  }
+  speak(fullSentence, { amplitude: 100, pitch: 30, speed: 135, wordgap: 5 });
+}
 
 function focusLine(num){
 
@@ -82,6 +109,7 @@ function tryScan(charStr){
       if (scanTime < SCAN_TIMEOUT){
         if (RFID.length == 5){
           currentStatus = StatusEnum.TYPING;
+          sentence[wordIndex]="";
           scanned();
         }
       }
@@ -98,6 +126,7 @@ function tryScan(charStr){
 //called when the RFID has been scanned and the user can type in the next input
 function scanned(){
   console.log("SCANNED");
+  speak("scanned", { amplitude: 100, pitch: 30, speed: 135, wordgap: 5 });
 }
 function isNumber(n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
@@ -105,15 +134,13 @@ function isNumber(n) {
 
 //change which input we're on, set focus to new input
 function nextWord(){
+  readSentence();
   //RESET 
   currentStatus = StatusEnum.READY;
   RFID="";
 	wordIndex = (wordIndex+1)%numWords;
-
-
   focusLine(wordIndex);
   hideLine(wordIndex);
-
   //if we've looped back to beginning, clear all words
 	if (wordIndex === 0){
 		clearAll();
@@ -126,5 +153,6 @@ function clearAll(){
 		$("#" + i).val("");
     $("#" + i).hide()
 	}
+  sentence = [];
   //$("#0").show();
 }
